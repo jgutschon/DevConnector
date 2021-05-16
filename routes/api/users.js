fd94d99529c2638b104e141bcd91218b83eed5e3
Jon Gutschon <jongutschon123@gmail.com>
2021-05-16 13:21:14 -0400

    authenticate in auth route

--- routes/api/users.js
+++ routes/api/users.js
@@ -71,7 +71,7 @@ router.post(
       );
 
       // Return JWT
-      res.send('User registered');
+      // res.send('User registered');
     } catch (err) {
       console.error(err.message);
       res.status(500).send('Server Error');
