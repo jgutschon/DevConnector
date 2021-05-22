21b34cbb14cc0358313d12f5121501568dac5671
Jon Gutschon <jongutschon123@gmail.com>
2021-05-22 14:17:17 -0400

    create user profile schema

---
+++ models/Profiles.js
@@ -0,0 +1,109 @@
+const mongoose = require('mongoose');
+
+const ProfileSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user',
+  },
+
+  company: {
+    type: String,
+  },
+  website: {
+    type: String,
+  },
+  location: {
+    type: String,
+  },
+  status: {
+    type: String,
+    required: true,
+  },
+  skills: {
+    type: [String],
+    required: true,
+  },
+  bio: {
+    type: String,
+  },
+  githubusername: {
+    type: String,
+  },
+  experience: [
+    {
+      title: {
+        type: String,
+        required: true,
+      },
+      company: {
+        type: String,
+        required: true,
+      },
+      location: {
+        type: String,
+      },
+      start: {
+        type: Date,
+        required: true,
+      },
+      finish: {
+        type: Date,
+      },
+      current: {
+        type: Boolean,
+      },
+      description: {
+        type: String,
+      },
+    },
+  ],
+  education: [
+    {
+      school: {
+        type: String,
+        required: true,
+      },
+      degree: {
+        type: String,
+        required: true,
+      },
+      fieldOfStudy: {
+        type: String,
+        required: true,
+      },
+      start: {
+        type: Date,
+        required: true,
+      },
+      finish: {
+        type: Date,
+      },
+      current: {
+        type: Boolean,
+      },
+    },
+  ],
+  social: {
+    linkedin: {
+      type: String,
+    },
+    facebook: {
+      type: String,
+    },
+    twitter: {
+      type: String,
+    },
+    instagram: {
+      type: String,
+    },
+    youtube: {
+      type: String,
+    },
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = Profile = mongoose.model('profile', ProfileSchema);
