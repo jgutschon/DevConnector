6156187bd3d5684d16f99462f90f713ff4afbaff
Jon Gutschon <jongutschon123@gmail.com>
2021-09-01 18:34:13 -0400

    add login page form

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,7 +1,75 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 const Login = () => {
-  return <Fragment>Login</Fragment>;
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  });
+
+  const { name, email, password } = formData;
+
+  const onChange = (event) =>
+    setFormData({ ...formData, [event.target.name]: event.target.value });
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    const user = {
+      email,
+      password,
+    };
+
+    try {
+      const config = {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      };
+
+      const body = JSON.stringify(user);
+      const res = await axios.post('/api/auth', body, config);
+
+      console.log(res.data);
+    } catch (err) {
+      console.error(err.response.data);
+    }
+  };
+
+  return (
+    <Fragment>
+      <h1 className="large text-primary">Sign Up</h1>
+      <p className="lead">
+        <i className="fas fa-user"></i> Create Your Account
+      </p>
+      <form className="form" onSubmit={(event) => onSubmit(event)}>
+        <div className="form-group">
+          <input
+            type="email"
+            placeholder="Email Address"
+            name="email"
+            value={email}
+            onChange={(event) => onChange(event)}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            value={password}
+            onChange={(event) => onChange(event)}
+            required
+          />
+        </div>
+        <input type="submit" className="btn btn-primary" value="Register" />
+      </form>
+      <p className="my-1">
+        Don't have an account? <Link to="/register">Register</Link>
+      </p>
+    </Fragment>
+  );
 };
 
 export default Login;
